73fbbdaecc48d5227fc48a8b305a8408a3f7b2bb
Lex Crawford <lexcraw4d@gmail.com>
2021-08-01 16:00:43 -0400

    code split app seperation of concerns

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,39 @@
 const path = require('path');
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+        
       },
-     plugins: [new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery"
-      })],
-    mode: 'development'
+      output: {
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist"
+      },
+      module: {
+        rules: [
+          {
+            test: /\.jpg$/i,
+            use: [
+              {
+                loader: 'file-loader'
+              }
+            ]
+          }
+        ]
+      },
+      plugins: [
+        new webpack.ProvidePlugin({
+          $: "jquery",
+          jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
+      ],  
 
 };
